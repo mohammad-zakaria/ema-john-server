5990c3871b1c490efb76eea06541406399e45877
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-04-01 08:42:12 +0600

    root api created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASS}@cluster0.8hqot.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8hqot.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 const app = express()
@@ -18,9 +18,9 @@ const port = 5000
 
 console.log(process.env.DB_User)
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
